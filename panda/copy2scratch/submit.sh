7ab1721ffba520147dd0e3cfea8412a1af5bdcf4
Nikolai Hartmann <nikoladze@posteo.de>
2021-06-29 08:48:55 +0200

    100% test on panda with copy2scratch

--- panda/copy2scratch/submit.sh
+++ panda/copy2scratch/submit.sh
@@ -1,9 +1,11 @@
 # standard setup for copy2scratch
 
-prun --containerImage /cvmfs/unpacked.cern.ch/registry.hub.docker.com/nikolaihartmann/physlite-experiments:latest \
-     --exec="python -m physlite_experiments.scripts.run_analysis_example %IN" \
-     --inDS=data17_13TeV.periodK.physics_Main.PhysCont.DAOD_PHYSLITE.grp17_v01_p4309 \
-     --outDS=user.nihartma.test_physlite_analysis_GCP_10p_copy2scratch_2021-05-27_1328 \
+inDS=$(cat ../data_dids.txt | tr "\n" "," | sed 's/,$//')
+
+prun --containerImage /cvmfs/unpacked.cern.ch/registry.hub.docker.com/nikolaihartmann/physlite-experiments:6f11b90 \
+     --exec="python -m physlite_experiments.scripts.run_analysis_example --max-chunksize 100000 %IN" \
+     --inDS="$inDS" \
+     --outDS=user.nihartma.test_physlite_analysis_GCP_100p_copy2scratch_2021-06-28_1130 \
      --nFilesPerJob 30 \
      --site=GOOGLE100 \
      --forceStaged --forceStagedSecondary \
